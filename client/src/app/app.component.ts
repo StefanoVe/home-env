f590223641cd6c725cdc46d025f3049571bb5951
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-09-25 14:30:32 +0200

    inzia impementazione applyconfig

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 export const clientVersion = '1.0.3b';
 
@@ -7,6 +9,39 @@ export const clientVersion = '1.0.3b';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public toggleDone = false;
+
+  constructor(
+    private _currentRouter: ActivatedRoute,
+    private _router: Router,
+    private _authService: AuthService
+  ) {}
+
+  public ngOnInit(): void {
+    this._applyConfig();
+  }
+
+  private _applyConfig(): void {
+    const apiKey = this._currentRouter.snapshot.queryParamMap.get('apiKey');
+    const targetAddress =
+      this._currentRouter.snapshot.queryParams['targetAddress'];
+
+    console.log(this._currentRouter.snapshot.queryParams);
+
+    if (apiKey) {
+      console.log('appling api key', apiKey);
+      this._authService.setAuth(apiKey);
+    }
+
+    if (targetAddress) {
+      console.log('appling target address', targetAddress);
+      this._authService.setTargetAddress(targetAddress);
+    }
+
+    if (apiKey || targetAddress) {
+      this._router.navigate(['/']);
+      location.reload();
+    }
+  }
 }
